0d46cb03a9093dd7ff9a30c1f3dea64a965fae22
ips912s29 <ips912s29@awp12.csc.local>
2020-04-12 02:02:45 +0700

    Implemented function of reading from file

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
-
-main()
+#include <cstring>
+#include <iostream>
+#include <fstream>
+using namespace std;
+void readFromFile(string arr[2][3], string filePath)
 {
+    //const 200
+    ifstream file(filePath);
+    for(int i=0;i<2;i++) {
+        for(int j=0;j<3;j++) {
+            getline(file,arr[i][j], ' ');
+            j++;
+            getline(file,arr[i][j], ' ');
+            j++;
+            getline(file,arr[i][j], '\n');
+        }
+    }
+}
+
+int main()
+{
+    
 	return 0;
 }
