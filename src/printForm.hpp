f59838ec9f9078db5341819ec1e6bea10ef7560b
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 20:24:44 +0700

    Add function

---
+++ src/printForm.hpp
@@ -0,0 +1,8 @@
+#ifndef FORM
+#define FORM
+
+void printRandomVerb(const string verbs_array[121][3], int i, const int *array);
+
+void printForm(const string verbs_array[121][3], int i, int j, const int *array);
+
+#endif
