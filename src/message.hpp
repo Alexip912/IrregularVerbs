ab87c80fbb158d590c1cc54aa6c77798275e854b
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 20:39:58 +0700

    Add function in header

---
+++ src/message.hpp
@@ -0,0 +1,8 @@
+#ifndef MESSAGE
+#define MESSAGE
+
+void message_right(int j, int &right);
+
+void message_wrong(const string verbs_array[121][3], int i, int j, const int *array, int &Simple, int &Participle, int &wrong, string &form, string &word);
+
+#endif
