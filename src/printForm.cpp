b18d85c41d15a9a48124d92ded15199f88e0af33
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 20:22:01 +0700

    Add function print

---
+++ src/printForm.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+void printRandomVerb(const string verbs_array[121][3], int i, const int *array) {
+    cout << "Your verb: " << verbs_array[array[i]][0];
+    cout << endl
+         << "Input other form" << endl;
+}
+
+void printForm(const string verbs_array[121][3], int i, int j, const int *array) {
+    if (j == 1) cout << "Past Simple: ";
+    if (j == 2) cout << "Past Participle: ";
+}
