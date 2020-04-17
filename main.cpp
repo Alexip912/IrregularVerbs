9aca080fbefcb5d9a5328961e60a2c77cfd91faa
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 16:28:38 +0700

    Add call new function

--- main.cpp
+++ main.cpp
@@ -128,14 +128,17 @@ void checkVerbs(const int *array, int size, const string verbs_array[121][3], in
         }
     }
 }
-
-int main()
-{	
-    int number_of_verbs;
+	
+int main() {
+    int number_of_verbs, wrongValue = 0, rightValue = 0;
     cout << "Enter number of verbs to check" << endl;
     cin >> number_of_verbs;
     int *a = new int[number_of_verbs];
+    randVerbs(a, number_of_verbs);
     string verbs_array[121][3];
+    string filePath = "verbs.txt";
+    readFromFile(verbs_array, filePath);
+    checkVerbs(a, number_of_verbs, verbs_array, wrongValue, rightValue);
     delete[] a;
-	return 0;
+    return 0;
 }
