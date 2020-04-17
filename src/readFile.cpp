216207fdd62363139f8cb22b8ba0cd135d322c64
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 20:15:41 +0700

    Add function readFromFile

---
+++ src/readFile.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <fstream>
+
+void readFromFile(string arr[121][3], const string &filePath) {
+    ifstream file(filePath);
+    for (int i = 0; i < 121; i++) {
+        for (int j = 0; j < 3; j++) {
+            getline(file, arr[i][j], ' ');
+            j++;
+            getline(file, arr[i][j], ' ');
+            j++;
+            getline(file, arr[i][j], '\n');
+        }
+    }
+}
