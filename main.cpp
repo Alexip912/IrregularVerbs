b95bf3e105b9e52024dadfc126c49aad12cb89a8
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 16:13:36 +0700

    Add function checkVerbs

--- main.cpp
+++ main.cpp
@@ -80,6 +80,27 @@ int equality_wrong(const string verbs_array[121][3], int i, int j, const int *ar
         return -1;
 }
 
+void checkVerbs(const int *array, int size, const string verbs_array[121][3], int &wrongValue, int &rightValue) {
+    string userVerb, form, word;
+    int Simple = 0, Participle = 0, right = 0, wrong = 0;
+    for (int i = 0; i < size; ++i) {
+        Simple = 0, Participle = 0, right = 0, wrong = 0;
+        printRandomVerb(verbs_array, i, array);
+        for (int j = 1; j < 3; ++j) {
+            printForm(verbs_array, i, j, array);
+            cin >> userVerb;
+            if (equality_right(verbs_array, i, j, array, userVerb, Simple, Participle, right) == 1) {
+                ++rightValue;
+            }
+            (message_right(j, right));
+            if (equality_wrong(verbs_array, i, j, array, userVerb, Simple, Participle, wrong, form, word) == 1) {
+                ++wrongValue;
+            }
+            message_wrong(verbs_array, i, j, array, Simple, Participle, wrong, form, word);
+        }
+    }
+}
+
 int main()
 {	
     int number_of_verbs;
