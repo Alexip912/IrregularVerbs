22c70f501fbf911c7d0a7935f9f4a15a84deec48
Petrov-Daniil <zara123486@mail.ru>
2020-04-17 16:19:04 +0700

    Add function message_right

--- main.cpp
+++ main.cpp
@@ -80,6 +80,13 @@ int equality_wrong(const string verbs_array[121][3], int i, int j, const int *ar
         return -1;
 }
 
+void message_right(int j, int &right) {
+    if (j == 2 && right == 2) {
+        cout << "All required forms entered correctly." << endl
+             << "Move on to the next." << endl;
+    }
+}
+
 void checkVerbs(const int *array, int size, const string verbs_array[121][3], int &wrongValue, int &rightValue) {
     string userVerb, form, word;
     int Simple = 0, Participle = 0, right = 0, wrong = 0;
