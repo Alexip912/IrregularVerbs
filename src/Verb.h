8f92dfea9c94be0652d986b42720acdc676928da
Petrov-Daniil <zara123486@mail.ru>
2020-04-30 22:02:07 +0700

    Added class information

---
+++ src/Verb.h
@@ -0,0 +1,33 @@
+#ifndef VERB
+#define VERB
+
+#include <string>
+
+class Verb {
+private:
+    std::string array_of_verbs[121][3];
+    std::string word;
+    std::string form;
+    std::string userVerb;
+    bool simple{};
+    bool participle{};
+    int right{};
+    int wrong{};
+    int index_i{};
+    int index_j{};
+    int wrong_value{};
+    int right_value{};
+
+public:
+    bool read_from_file(const std::string& filePath);
+    static void rand_verbs(int* array, int size);
+    void check_verbs(const int* array, int size);
+    bool equality_right(const int* array);
+    bool equality_wrong(const int* array);
+
+private:
+    void message_right() const;
+    void message_wrong(const int* array);
+    void print_form() const;
+    void print_random_verb(const int* array);
+};
