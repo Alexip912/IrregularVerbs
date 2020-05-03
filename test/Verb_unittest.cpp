a1f80bcce44867ee8d4ef1f8d5306aa8018d9f2e
Petrov-Daniil <zara123486@mail.ru>
2020-05-03 22:29:06 +0700

    Created new test case eq_wrong and added test IsWrong

--- test/Verb_unittest.cpp
+++ test/Verb_unittest.cpp
@@ -43,3 +43,23 @@ TEST(EQUALITY_RIGHT, IsNotRight)
     test.Set_index_j(0);
     ASSERT_EQ(false, test.equality_right(array_for_test));
 }
+
+TEST(EQUALITY_WRONG, IsWrong)
+{
+    int array_for_test[3] = {0, 1, 2};
+    Verb test;
+    string filePath = "res/verbs.txt";
+    test.read_from_file(filePath);
+    test.Set_user_verb("be");
+    test.Set_index_i(0);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_wrong(array_for_test));
+    test.Set_user_verb("was");
+    test.Set_index_i(1);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_wrong(array_for_test));
+    test.Set_user_verb("been");
+    test.Set_index_i(2);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_wrong(array_for_test));
+}
