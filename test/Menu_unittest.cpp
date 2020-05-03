544b0fe3ea3451374babc1a99025cb7b3c7c445b
Petrov-Daniil <zara123486@mail.ru>
2020-05-03 23:11:07 +0700

    Created new test case choice and added test IsChoice

--- test/Menu_unittest.cpp
+++ test/Menu_unittest.cpp
@@ -16,3 +16,10 @@ TEST(ABOUT, IsNotAbout)
     ASSERT_EQ(false, check_file("235456.txt"));
     ASSERT_EQ(false, check_file("home/about.txt"));
 }
+
+TEST(CHOICE, IsChoice)
+{
+    ASSERT_EQ(true, is_valid_choice("1"));
+    ASSERT_EQ(true, is_valid_choice("2"));
+    ASSERT_EQ(true, is_valid_choice("3"));
+}
