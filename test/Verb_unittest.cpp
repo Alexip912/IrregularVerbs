c343892fd9315618b6128375b0875a04630636b2
Petrov-Daniil <zara123486@mail.ru>
2020-05-03 22:24:52 +0700

    Created new test case eq_right and added test IsRight

---
+++ test/Verb_unittest.cpp
@@ -0,0 +1,25 @@
+#include "header/Verb.h"
+#include <gtest/gtest.h>
+#include <string>
+
+using namespace std;
+
+TEST(EQUALITY_RIGHT, IsRight)
+{
+    int array_for_test[3] = {0, 1, 2};
+    Verb test;
+    string filePath = "res/verbs.txt";
+    test.read_from_file(filePath);
+    test.Set_user_verb("arise");
+    test.Set_index_i(0);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_right(array_for_test));
+    test.Set_user_verb("awake");
+    test.Set_index_i(1);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_right(array_for_test));
+    test.Set_user_verb("be");
+    test.Set_index_i(2);
+    test.Set_index_j(0);
+    ASSERT_EQ(true, test.equality_right(array_for_test));
+}
