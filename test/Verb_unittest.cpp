46978cb52cbc0919dcef34bfb3b58c08a4a00086
Petrov-Daniil <zara123486@mail.ru>
2020-05-03 22:30:33 +0700

    Added new test IsNotWrong

--- test/Verb_unittest.cpp
+++ test/Verb_unittest.cpp
@@ -63,3 +63,23 @@ TEST(EQUALITY_WRONG, IsWrong)
     test.Set_index_j(0);
     ASSERT_EQ(true, test.equality_wrong(array_for_test));
 }
+
+TEST(EQUALITY_WRONG, IsNotWrong)
+{
+    int array_for_test[3] = {0, 1, 2};
+    Verb test;
+    string filePath = "res/verbs.txt";
+    test.read_from_file(filePath);
+    test.Set_user_verb("arose");
+    test.Set_index_i(0);
+    test.Set_index_j(1);
+    ASSERT_EQ(false, test.equality_wrong(array_for_test));
+    test.Set_user_verb("awoke");
+    test.Set_index_i(1);
+    test.Set_index_j(1);
+    ASSERT_EQ(false, test.equality_wrong(array_for_test));
+    test.Set_user_verb("was");
+    test.Set_index_i(2);
+    test.Set_index_j(1);
+    ASSERT_EQ(false, test.equality_wrong(array_for_test));
+}
