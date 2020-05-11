fd63a1fa47577c6c2e442ab5c2a821e96cd442b6
Petrov-Daniil <zara123486@mail.ru>
2020-05-11 19:37:40 +0700

    Changed type of fuctions

--- src/header/Verb.h
+++ src/header/Verb.h
@@ -11,13 +11,13 @@ typedef struct {
 extern bool simple, participle;
 extern std::string array_of_verbs[121][3];
 
-bool read_from_file(const std::string& filePath);
+void read_from_file(const std::string& filePath);
 void rand_verbs(int* array, const int& size);
-int check_verbs(const int* array, const int& size, DataOfCurrentVerb& object);
+bool check_verbs(
+        const std::string& reference_verb, const std::string& user_verb);
 int result(const int& number_of_verbs, const int& right_value);
 void message_right();
 void message_wrong(const int& index, const DataOfCurrentVerb& object);
 void print_form(const int& index);
 void print_random_verb(const int& index);
-
 #endif
