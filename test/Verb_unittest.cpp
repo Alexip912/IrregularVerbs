16adc03fbb243165d4b5782ddcc720d65ac0f80f
Petrov-Daniil <zara123486@mail.ru>
2020-05-11 19:55:03 +0700

    Created new test case CHECK and deleted test case READ

--- test/Verb_unittest.cpp
+++ test/Verb_unittest.cpp
@@ -4,29 +4,30 @@
 
 using namespace std;
 
-TEST(READ_FROM_FILE, IsRead)
+TEST(CHECK, IsRight)
 {
-    ASSERT_EQ(true, read_from_file("res/verbs.txt"));
+    ASSERT_EQ(true, check_verbs("be", "be"));
+    ASSERT_EQ(true, check_verbs("was", "was"));
+    ASSERT_EQ(true, check_verbs("been", "been"));
 }
 
-TEST(READ_FROM_FILE, IsNotRead)
+TEST(CHECK, IsWrong)
 {
-    ASSERT_EQ(false, read_from_file(""));
-    ASSERT_EQ(false, read_from_file("hfjhfjhf.txt"));
-    ASSERT_EQ(false, read_from_file("1234.txt"));
-    ASSERT_EQ(false, read_from_file("home/verbs.txt"));
+    ASSERT_EQ(false, check_verbs("be", "can"));
+    ASSERT_EQ(false, check_verbs("be", "was"));
+    ASSERT_EQ(false, check_verbs("been", "was"));
 }
 
 TEST(RESULT, IsResult)
 {
-    ASSERT_EQ(1, result(100,2));
-    ASSERT_EQ(100, result(25,50));
-    ASSERT_EQ(25, result(20,10));
+    ASSERT_EQ(1, result(100, 2));
+    ASSERT_EQ(100, result(25, 50));
+    ASSERT_EQ(25, result(20, 10));
 }
 
 TEST(RESULT, IsNotResult)
 {
-    ASSERT_EQ(0, result(0,0));
-    ASSERT_EQ(0, result(-5,2));
-    ASSERT_EQ(0, result(-25,7));
+    ASSERT_EQ(0, result(0, 0));
+    ASSERT_EQ(0, result(-5, 2));
+    ASSERT_EQ(0, result(-25, 7));
 }
