f183cafd2a4b2795cf3ce9d9f209ddccdf9f6110
Petrov-Daniil <zara123486@mail.ru>
2020-05-15 13:55:39 +0700

    Deleted old structure DataOfCurrentVerb and created new structure

--- src/header/Verb.h
+++ src/header/Verb.h
@@ -2,22 +2,24 @@
 #define VERB
 
 #include <string>
+#include <vector>
 
 typedef struct {
-    std::string word;
-    std::string form;
-} DataOfCurrentVerb;
+    std::string infinitive;
+    std::string simple;
+    std::string participle;
+    bool simple_flag;
+    bool participle_flag;
+} FormsOfVerb;
 
-extern bool simple, participle;
-extern std::string array_of_verbs[121][3];
-
-void read_from_file(const std::string& filePath);
-void rand_verbs(int* array, const int& size);
+void read_from_file(
+        const std::string& file_path, std::vector<FormsOfVerb>& array_of_verbs);
+void rand_verbs(int* array, int size);
 bool check_verbs(
         const std::string& reference_verb, const std::string& user_verb);
-int result(const int& number_of_verbs, const int& right_value);
-void message_right();
-void message_wrong(const int& index, const DataOfCurrentVerb& object);
-void print_form(const int& index);
-void print_random_verb(const int& index);
+int result(int number_of_verbs, int right_value);
+void message_right(const FormsOfVerb& verb);
+void message_wrong(const FormsOfVerb& verb);
+void print_form(int index);
+void print_random_verb(const FormsOfVerb& verb);
 #endif
