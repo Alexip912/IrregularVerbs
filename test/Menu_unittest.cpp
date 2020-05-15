117b320b780d858ad833d6d85f966cea3f1c1f75
Petrov-Daniil <zara123486@mail.ru>
2020-05-15 13:58:54 +0700

    False to null and true to one

--- test/Menu_unittest.cpp
+++ test/Menu_unittest.cpp
@@ -6,51 +6,52 @@ using namespace std;
 
 TEST(CHOICE, IsChoice)
 {
-    ASSERT_EQ(true, is_valid_choice("1"));
-    ASSERT_EQ(true, is_valid_choice("2"));
-    ASSERT_EQ(true, is_valid_choice("3"));
+    ASSERT_EQ(1, is_valid_choice("1"));
+    ASSERT_EQ(1, is_valid_choice("2"));
+    ASSERT_EQ(1, is_valid_choice("3"));
 }
 
 TEST(CHOICE, IsNotChoice)
 {
-    ASSERT_EQ(false, is_valid_choice(""));
-    ASSERT_EQ(false, is_valid_choice("222"));
-    ASSERT_EQ(false, is_valid_choice("fghfghfh"));
-    ASSERT_EQ(false, is_valid_choice("f"));
-    ASSERT_EQ(false, is_valid_choice("2d"));
-    ASSERT_EQ(false, is_valid_choice("1235448"));
-    ASSERT_EQ(false, is_valid_choice("!"));
-    ASSERT_EQ(false, is_valid_choice("."));
+    ASSERT_EQ(0, is_valid_choice(""));
+    ASSERT_EQ(0, is_valid_choice("222"));
+    ASSERT_EQ(0, is_valid_choice("fghfghfh"));
+    ASSERT_EQ(0, is_valid_choice("f"));
+    ASSERT_EQ(0, is_valid_choice("2d"));
+    ASSERT_EQ(0, is_valid_choice("1235448"));
+    ASSERT_EQ(0, is_valid_choice("!"));
+    ASSERT_EQ(0, is_valid_choice("."));
 }
 
 TEST(NUMBER, IsNumber)
 {
-    ASSERT_EQ(true, is_valid_number("0"));
-    ASSERT_EQ(true, is_valid_number("12345"));
-    ASSERT_EQ(true, is_valid_number("8"));
+    ASSERT_EQ(1, is_valid_number("0"));
+    ASSERT_EQ(1, is_valid_number("12345"));
+    ASSERT_EQ(1, is_valid_number("8"));
 }
 
 TEST(NUMBER, IsNotNumber)
 {
-    ASSERT_EQ(false, is_valid_number(""));
-    ASSERT_EQ(false, is_valid_number("-1"));
-    ASSERT_EQ(false, is_valid_number("fd"));
-    ASSERT_EQ(false, is_valid_number("4gfjf"));
-    ASSERT_EQ(false, is_valid_number("!"));
-    ASSERT_EQ(false, is_valid_number("."));
+    ASSERT_EQ(0, is_valid_number(""));
+    ASSERT_EQ(0, is_valid_number("-1"));
+    ASSERT_EQ(0, is_valid_number("fd"));
+    ASSERT_EQ(0, is_valid_number("4gfjf"));
+    ASSERT_EQ(0, is_valid_number("!"));
+    ASSERT_EQ(0, is_valid_number("."));
 }
 
 TEST(CHECK_FILE, IsRead)
 {
-    ASSERT_EQ(true, check_file("res/about.txt"));
+    ASSERT_EQ(1, check_file("res/about.txt"));
+    ASSERT_EQ(1, check_file("res/verbs.txt"));
 }
 
 TEST(CHECK_FILE, IsNotRead)
 {
-    ASSERT_EQ(false, check_file(""));
-    ASSERT_EQ(false, check_file("hfjhfjhf.txt"));
-    ASSERT_EQ(false, check_file("1234.txt"));
-    ASSERT_EQ(false, check_file("home/verbs.txt"));
+    ASSERT_EQ(0, check_file(""));
+    ASSERT_EQ(0, check_file("hfjhfjhf.txt"));
+    ASSERT_EQ(0, check_file("1234.txt"));
+    ASSERT_EQ(0, check_file("home/verbs.txt"));
 }
 
 TEST(STONUM, IsNotNull)
