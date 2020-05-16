e8866b3685530d8a43f0b420bb96a1dbe5810e8b
Фадин Владислав Максимович <sad_456@ngs.ru>
2020-05-16 23:45:41 +0700

    Extended (CHECK, 'IsRight)' case

--- test/Verb_unittest.cpp
+++ test/Verb_unittest.cpp
@@ -13,6 +13,18 @@ TEST(CHECK, IsRight)
     word = "been";
     user_verbs.emplace_back(word);
     ASSERT_EQ(2, check_verbs(user_verbs, verbs[2]));
+    user_verbs.clear();
+    word = "became";
+    user_verbs.emplace_back(word);
+    word = "become";
+    user_verbs.emplace_back(word);
+    ASSERT_EQ(2, check_verbs(user_verbs, verbs[6]));
+    user_verbs.clear();
+    word = "wound";
+    user_verbs.emplace_back(word);
+    word = "wount";
+    user_verbs.emplace_back(word);
+    ASSERT_EQ(1, check_verbs(user_verbs, verbs[119]));
 }
 
 TEST(CHECK, IsNotRight)
